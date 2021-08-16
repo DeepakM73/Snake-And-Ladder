c7fb82f382eff035ad8234c51180fc1dc5c57c5e
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-16 06:17:00 +0530

    Dice

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -12,11 +12,14 @@ public class SnakeAndLadder {
          */
 
           /**
-          * @param Start -Is the starting Point
+          * @param start -Is the starting Point
+           * @param dice -Is random function to generate dice output i.e 1-6
           */
 
         int start=0;
         System.out.println("Welcome to Snake And Ladder game");
         System.out.println("\nStarting the game with Point= " + start);
+        int dice = (int)(Math.random() * 6) + 1;
+        System.out.println("number on the dice is :" + dice);
     }
 }

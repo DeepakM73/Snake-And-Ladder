e979d64476b04b2594f3208b34b9be26226fe270
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-16 05:58:51 +0530

    Welcome

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+        /**
+         * The following implements snake and ladder game
+         *
+         * @author Deepak
+         * @version 1.0
+         * @since 13/08/2021
+         */
+
+          /**
+          * @param Start -Is the starting Point
+          */
+
+        int start=0;
+        System.out.println("Welcome to Snake And Ladder game");
+        System.out.println("\nStarting the game with Point= " + start);
+    }
+}
